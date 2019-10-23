36800158f3987915cafbbe57c273e107da8c6d5a
bhu90klmg <56907566+bhu90klmg@users.noreply.github.com>
2019-10-23 23:25:09 +0800

    n*n

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,12 @@ public class Main {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         int sum = 0;
-        for (int i =1 ; i<=n ; i++){
-            sum = sum+i*(i+1);
+        for (int i = 1 ; i<=n ; i++){
+            for (int a = 1 ; a<=n ; a++){
+                System.out.printf("%d\t",i*a);
+            }
+            System.out.println(" ");
         }
-        System.out.println(sum);
     }
 }
 
