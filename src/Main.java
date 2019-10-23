cd185f34720204e99cd275e0c57f06d23e7a77c1
bhu90klmg <56907566+bhu90klmg@users.noreply.github.com>
2019-10-23 23:29:39 +0800

    n*n

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        int sum = 0;
         for (int i = 1 ; i<=n ; i++){
             for (int a = 1 ; a<=n ; a++){
                 System.out.printf("%d\t",i*a);
