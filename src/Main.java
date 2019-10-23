47f21c2d1f6d929e2afca1a400b0717e923ee305
bhu90klmg <56907566+bhu90klmg@users.noreply.github.com>
2019-10-23 22:05:16 +0800

    最大值與最小值

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        float v1 = scn.nextFloat();
+        float max = v1;
+        float min = v1;
+        for (int i=1; i<10; i++){
+            v1 = scn.nextFloat();
+            if (max<v1){
+                max = v1;
+            }
+            else if (min>v1){
+                min = v1;
+            }
+        }
+        System.out.printf("max=%.2f\n",max);
+        System.out.printf("min=%.2f\n",min);
+    }
+}
+
