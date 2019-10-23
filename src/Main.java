bffca2a0e664febeb37f243bc1c55a39d5346a1e
bhu90klmg <56907566+bhu90klmg@users.noreply.github.com>
2019-10-23 23:05:41 +0800

    質數判別

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,24 @@ import java.util.Scanner;
 
 public class Main {
 
-
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        int N =scn.nextInt();
-        int sum = 0;
-        for(int i= 1 ; i<=N; i++){
-            if (i%3==0){
-                sum = sum +i;
+        int n = scn.nextInt();
+        int m = n/2;
+        boolean flag = true;
+        for (int i = 2 ;i<=m;i++){
+            if (n%i==0){
+                flag = false;
+                break;
             }
         }
-        System.out.println(sum);
+        if (flag){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+
+        }
     }
 }
 
