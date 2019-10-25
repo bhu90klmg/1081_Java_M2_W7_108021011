9415c74d284475cc1f958ba4d4e3cebe4a7a7ff7
bhu90klmg <56907566+bhu90klmg@users.noreply.github.com>
2019-10-25 22:11:41 +0800

    實心 正方形

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        for (int  i= 1 ; i<=n; i++){
-            if (n%i==0){
-                System.out.println(i);
+        char b = scn.next().charAt(0);
+        for (int i = 1; i <= n; i++) {
+            for (int a = 1; a <= n; a++) {
+                System.out.print(b);
             }
+            System.out.println(" ");
         }
     }
 }
+
