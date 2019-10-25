63fa7c013acbe32a7a93a3411366bae51b8d4a5d
bhu90klmg <56907566+bhu90klmg@users.noreply.github.com>
2019-10-25 19:26:37 +0800

    n!

--- src/Main.java
+++ src/Main.java
@@ -2,35 +2,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int ans = 0;
-        Scanner scn = new Scanner( System.in );
-        int x = scn.nextInt();
-        for (int z = 1;z <= x;z++){//將所有數字拿去測試
-            boolean ck235 = false;
-            for(int i=2; i<=z; i++){
-                if(z%i==0){//可以整除->i是z的因數
-                    boolean check = true;
-                    for(int j=2; check && j<i; j++){
-                        if(i%j==0){//i可以被整除->i不是質因數
-                            check = false;
-                        }
-                    }
-                    if(check){
-
-                        if (i==2 || i==3 || i==5){//判斷質因數是否為2或3或5
-                            ck235 = true;
-                        }else {
-                            ck235 = false;
-
-                        }
-                    }
-                }
-            }
-
-            if (ck235){
-                ans = ans + z;
-            }
+        Scanner scn = new Scanner(System.in);
+        long n = scn.nextInt();
+        long a = 1;
+        for (long i = 1; i<=n ; i++ ){
+            a = a*i;
         }
-        System.out.println(ans);
+        System.out.println(a);
     }
 }
