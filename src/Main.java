aba2b4c3f433518c601f805868c2e1caad85fb98
bhu90klmg <56907566+bhu90klmg@users.noreply.github.com>
2019-10-25 22:36:21 +0800

    2的n次總和

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        char b = scn.next().charAt(0);
-        for (int i = 1; i <= n; i++) {
-            for (int a = 1; a <= n; a++) {
-                System.out.print(b);
-            }
-            System.out.println(" ");
+        int sum=0;
+        int b ;
+        for (int i=1; i<=n; i++){
+             b = (int) Math.pow(2,i);
+            sum = sum +b ;
         }
+        System.out.println(sum);
     }
 }
 
